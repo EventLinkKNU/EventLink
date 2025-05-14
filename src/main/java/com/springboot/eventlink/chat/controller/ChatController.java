837a54dbf7086c057c993aa01c04353930444403
chajihyun <gh8526@hanmail.net>
2025-05-14 20:26:56 +0900

    chat read, create
    채팅 조회 및 생성

---
+++ src/main/java/com/springboot/eventlink/chat/controller/ChatController.java
@@ -0,0 +1,39 @@
+package com.springboot.eventlink.chat.controller;
+
+import com.springboot.eventlink.chat.dto.ChatMessageDto;
+import com.springboot.eventlink.chat.entity.Chat;
+import com.springboot.eventlink.chat.service.ChatService;
+import com.springboot.eventlink.user.entity.Users;
+import com.springboot.eventlink.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/chats")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+    private final UserRepository userRepository;
+
+    //사용자가 참여 중인 채팅방 리스트 보기
+    @PostMapping("/{userId}")
+    public ResponseEntity<List<Chat>> getUserUserChatRooms(@PathVariable("userId") Long userId) {
+        List<Chat> chatList = chatService.getChatByUserId(userId);
+        return ResponseEntity.ok(chatList);
+    }
+
+    //이벤트 상세 화면에서 채팅 시작 (방 생성 or 기존 방 반환)
+    @PostMapping("/create/{userId}")
+    public ResponseEntity<ChatMessageDto> createChatRoom(@PathVariable("userId") Long userId, @RequestBody ChatMessageDto request) {
+        Users sender = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("보내는 사용 없음"));
+
+        Users receiver = userRepository.findById(request.getReceiveId()).orElseThrow(() -> new IllegalArgumentException("받는 사용자 없음"));
+
+        ChatMessageDto result = chatService.createOrReturnChatRoom(sender, receiver);
+        return ResponseEntity.ok(result);
+    }
+}
--- src/main/java/com/springboot/eventlink/chat/repository/ChatRepository.java
+++ src/main/java/com/springboot/eventlink/chat/repository/ChatRepository.java
@@ -1,7 +1,19 @@
 package com.springboot.eventlink.chat.repository;
 
 import com.springboot.eventlink.chat.entity.Chat;
+import com.springboot.eventlink.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+    Optional<Chat> findTopBySendIDAndReceiveIDOrReceiveIDAndSendIDOrderByCreatedAtDesc(
+            Users send1, Users receive1,
+            Users send2, Users receive2
+    );
+
+    List<Chat> findAllBySendID_IdOrReceiveID_Id(Long sendId, Long receiveId);
+
+
 }
